dd6a91049db387d9557fd105812455d8f4e4fde2
danarab4 <danarab4@mail.huji.ac.il>
2020-04-01 15:55:36 +0300

    changed the number of iterations per action

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "osm.h"
 
 int main() {
-	std::cout << osm_operation_time(10000000) << '\n';
-	std::cout <<osm_function_time(10000000) << '\n';
-	std::cout <<osm_syscall_time(10000000) << '\n';
+	std::cout << osm_operation_time(100000) << '\n';
+	std::cout <<osm_function_time(100000) << '\n';
+	std::cout << osm_syscall_time(100000) << '\n';
 };
