558221d3a4c95a24d13cd4ca48cb41f819f19fc3
danarab4 <danarab4@mail.huji.ac.il>
2020-04-01 13:00:54 +0300

    addition operation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-//
-// Created by danar on 01-Apr-20.
-//
-
 #include "main.hpp"
+#include <iostream>
+#include "osm.cpp"
+#include "osm.h"
+
+int main() {
+	std::cout << osm_operation_time(10000000);
+//	std::cout  << "1" ;
+};
--- osm.cpp
+++ osm.cpp
@@ -1,4 +1,63 @@
-//
-// Created by danarab4 on 01/04/2020.
-//
+#include "osm.h"
+#include <sys/time.h>
+#include <iostream>
+#include <cmath>
 
+/* Time measurement function for a simple arithmetic operation.
+   returns time in nano-seconds upon success,
+   and -1 upon failure.
+   */
+double osm_operation_time(unsigned int iterations)
+{
+	timeval curr_time;
+	int retval;
+	retval = gettimeofday(&curr_time, NULL);
+	if (retval == -1 or iterations == 0)
+	{
+		return -1;
+	}
+	double t1 = curr_time.tv_usec;
+	iterations = iterations / 5;
+	int var;
+	for (int i = 0; i < iterations; ++i)
+	{
+		var = i + i;
+		var = i + i;
+		var = i + i;
+		var = i + i;
+		var = i + i;
+	}
+	retval = gettimeofday(&curr_time, NULL);
+	double t2 = curr_time.tv_usec;
+	if (retval != -1)
+	{
+		return ((t2 - t1) / iterations) * pow(10,9);
+	}
+	return -1;
+}
+
+
+/* Time measurement function for an empty function call.
+   returns time in nano-seconds upon success,
+   and -1 upon failure.
+   */
+double osm_function_time(unsigned int iterations)
+{
+	return 1;
+}
+
+
+/* Time measurement function for an empty trap into the operating system.
+   returns time in nano-seconds upon success,
+   and -1 upon failure.
+   */
+double osm_syscall_time(unsigned int iterations)
+{
+	return 1;
+}
+
+//int main() {
+//	std::cout << osm_operation_time(10000000);
+//	return 1;
+////	std::cout  << "1" ;
+//};
