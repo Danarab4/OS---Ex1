169754bd1cb8ece14cb61d38394a28abad8c3dcd
Dror Bar <drorspace94@gmail.com>
2020-04-03 12:18:09 +0300

    update way to calculate time

--- osm.cpp
+++ osm.cpp
@@ -11,35 +11,42 @@ void empty_function()
 {
 }
 
+double timeval_to_nano(timeval time_obj){
+	return time_obj.tv_sec * pow(10,9) + time_obj.tv_usec * pow(10,3);
+}
+
 /* Time measurement function for a simple arithmetic operation.
    returns time in nano-seconds upon success,
    and -1 upon failure.
    */
 double osm_operation_time(unsigned int iterations)
 {
-	timeval curr_time;
+	timeval start_time, end_time;
 	int retval;
-	retval = gettimeofday(&curr_time, NULL);
+	double diff;
+	unsigned int var = 0;
+
+	retval = gettimeofday(&start_time, NULL);
 	if (retval == -1 or iterations == 0)
 	{
 		return -1;
 	}
-	double t1 = curr_time.tv_usec;
-	unsigned int var = 0;
-	while (var < iterations)
+
+	for (unsigned int i = 0; i < iterations;)
 	{
-		var += 1;
-		var += 1;
-		var += 1;
-		var += 1;
-		var += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
+		i += 1;
 	}
 
-	retval = gettimeofday(&curr_time, NULL);
-	double t2 = curr_time.tv_usec;
+	retval = gettimeofday(&end_time, NULL);
 	if (retval != -1)
 	{
-		return ((t2 - t1) / iterations) * pow(10,9);
+
+		diff = timeval_to_nano(end_time) - timeval_to_nano(start_time);
+		return diff / iterations;
 	}
 	return -1;
 }
@@ -51,16 +58,17 @@ double osm_operation_time(unsigned int iterations)
    */
 double osm_function_time(unsigned int iterations)
 {
-	timeval curr_time;
+	timeval start_time, end_time;
 	int retval;
-	retval = gettimeofday(&curr_time, NULL);
+	double diff ;
+
+	retval = gettimeofday(&start_time, NULL);
 	if (retval == -1 or iterations == 0)
 	{
 		return -1;
 	}
-	double t1 = curr_time.tv_usec;
-	iterations = iterations / 5;
-	for (unsigned int i = 0; i < iterations; ++i)
+
+	for (unsigned int i = 0; i < iterations; i=i+5)
 	{
 		empty_function();
 		empty_function();
@@ -68,11 +76,11 @@ double osm_function_time(unsigned int iterations)
 		empty_function();
 		empty_function();
 	}
-	retval = gettimeofday(&curr_time, NULL);
-	double t2 = curr_time.tv_usec;
+	retval = gettimeofday(&end_time, NULL);
 	if (retval != -1)
 	{
-		return ((t2 - t1) / iterations) * pow(10,9);
+		diff = timeval_to_nano(end_time) - timeval_to_nano(start_time);
+		return diff / iterations;
 	}
 	return -1;
 }
@@ -85,16 +93,16 @@ double osm_function_time(unsigned int iterations)
 double osm_syscall_time(unsigned int iterations)
 {
 
-	timeval curr_time;
+	timeval start_time, end_time;
+	double diff;
 	int retval;
-	retval = gettimeofday(&curr_time, NULL);
+	retval = gettimeofday(&start_time, NULL);
 	if (retval == -1 or iterations == 0)
 	{
 		return -1;
 	}
-	double t1 = curr_time.tv_usec;
-	iterations = iterations / 5;
-	for (unsigned int i = 0; i < iterations; ++i)
+
+	for (unsigned int i = 0; i < iterations; i=i+5)
 	{
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
@@ -102,11 +110,11 @@ double osm_syscall_time(unsigned int iterations)
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
 	}
-	retval = gettimeofday(&curr_time, NULL);
-	double t2 = curr_time.tv_usec;
+	retval = gettimeofday(&end_time, NULL);
 	if (retval != -1)
 	{
-		return ((t2 - t1) / iterations) * pow(10,9);
+		diff = timeval_to_nano(end_time) - timeval_to_nano(start_time);
+		return diff / iterations;
 	}
 	return -1;
 }
